0154cf33381ca4906742edaa8368b2a872f409fd
py563 <maredukondaprajwal@gmail.com>
2021-10-14 18:16:13 +0530

    made improvements using where clause for filter

--- L4-ActiveRecord/todo.rb
+++ L4-ActiveRecord/todo.rb
@@ -2,43 +2,40 @@ require "date"
 require "active_record"
 
 class Todo < ActiveRecord::Base
-  def due_today?
+  def is_due_today?
     due_date == Date.today
-  end
-
-  def overdue?
-    due_date < Date.today
-  end
-
-  def due_later?
-    due_date > Date.today
   end
 
   def to_displayable_string
     display_status = completed ? "[X]" : "[ ]"
-    display_date = due_today? ? nil : due_date
+    display_date = is_due_today? ? nil : due_date
     "#{id}. #{display_status} #{todo_text} #{display_date}"
   end
 
-  def self.to_displayable_list
-    all.map { |todo| todo.to_displayable_string }
+  def self.due_today?
+    where("due_date = ?", Date.today)
+  end
+
+  def self.due_later?
+    where("due_date > ?", Date.today)
+  end
+
+  def self.overdue?
+    where("due_date < ?", Date.today)
   end
 
   def self.show_list
     puts "My Todo-list\n\n"
     puts "Overdue \n"
-    all.filter { |todo_item| todo_item.overdue? }
-      .map { |todo_item| puts todo_item.to_displayable_string }
+    overdue?.map { |todo_item| puts todo_item.to_displayable_string }
 
     puts "\n\n"
     puts "Due Today\n"
-    all.filter { |todo_item| todo_item.due_today? }
-      .map { |todo_item| puts todo_item.to_displayable_string }
+    due_today?.map { |todo_item| puts todo_item.to_displayable_string }
 
     puts "\n\n"
     puts "Due Later\n"
-    all.filter { |todo_item| todo_item.due_later? }
-      .map { |todo_item| puts todo_item.to_displayable_string }
+    due_later?.map { |todo_item| puts todo_item.to_displayable_string }
   end
 
   def self.add_task(todo_item)
